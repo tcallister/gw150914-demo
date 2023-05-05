8de063c2bb7dc8e4e82cffa03a7be28b45d78d6f
Tom Callister <thomas.a.callister@gmail.com>
2023-05-04 20:26:12 -0500

    Adding script to download data

---
+++ data/get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -o H-H1_GWOSC_4KHZ_R1-1126259447-32.gwf https://gwosc.org/eventapi/html/GWTC-1-confident/GW150914/v3/H-H1_GWOSC_4KHZ_R1-1126259447-32.gwf
+curl -o L-L1_GWOSC_4KHZ_R1-1126259447-32.gwf https://gwosc.org/eventapi/html/GWTC-1-confident/GW150914/v3/L-L1_GWOSC_4KHZ_R1-1126259447-32.gwf
+curl -o GW150914_GWTC-1_PE.hdf5 https://dcc.ligo.org/public/0157/P1800370/005/GW150914_GWTC-1.hdf5
